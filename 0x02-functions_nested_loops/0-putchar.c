7520499a4bf7604ed610fd7622feddac91c5e17a
VSOLUTIONSE <femmaje@gmail.com>
2022-04-13 08:33:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putcahr('r');
+	_putchar('\n');
+
+	return (0);
+}
