9f032c265fb4ccfca02ded07d79ea9509d8c2b17
VSOLUTIONSE <femmaje@gmail.com>
2022-04-14 10:44:43 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <"main.h">
+
+/**
+ * main - test functions that print if intergers are positive or negative
+ * Rturn: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
